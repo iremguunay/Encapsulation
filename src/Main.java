c9e6e8e7fbe0f61979525526f7143ab205220ad1
Irem Gunay <iremguunay@gmail.com>
2022-03-21 21:13:03 +0300

    Implementing encapsulation with access modifiers

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree myFavoriteMapleTree = new Tree(90,
+                30, TreeType.MAPLE);
+
+        System.out.println(myFavoriteMapleTree.getTreeType());
+        System.out.println(myFavoriteMapleTree.getHeightFt());
+        System.out.println(myFavoriteMapleTree.getTrunkDiameterInches());
+
+        myFavoriteMapleTree.announceTallTree();
+
+        //myFavoriteMapleTree.heightFt = 180;
+        myFavoriteMapleTree.grow();
+    }
+}
